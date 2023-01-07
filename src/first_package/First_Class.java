5dadb9e9465f06a5b3bbe0d2e1f35b174fcedb3f
irish20kova <114425907+irish20kova@users.noreply.github.com>
2023-01-07 14:32:05 -0500

    Update First_Class.java

--- src/first_package/First_Class.java
+++ src/first_package/First_Class.java
@@ -3,5 +3,7 @@ package first_package;
 public class First_Class {
     public static void main(String[] args) {
         System.out.println("First Class");
+        //changes are made 
+        System.out.println("First Class");
     }
 }
